45a047c0be5d4f65d343a0cb11113a793bd72ae4
Manpreet Singh <123541359+manpreet-singh47@users.noreply.github.com>
2024-01-24 22:54:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "./components/container";
+
+const App = () => {
+  return (
+    <>
+      hii
+      <Container>
+        <input />
+      </Container>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div class="form-outline" data-mdb-input-init>
+      <input
+        type="text"
+        id="form12"
+        class="form-control "
+        placeholder="hiiiiiii"
+      />
+      <label class="form-label" for="form12">
+        Example label
+      </label>
+    </div>
+  );
+};
+
+export default Input;
